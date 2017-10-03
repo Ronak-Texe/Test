5f1fb6d17c606019e255a2ceb8fddf6088c922c3
Ronak Sharma <ronaksharma@pc-145-116.wireless.shef.omnia.lan>
2017-10-03 08:20:35 +0100

    added UDP.py

---
+++ UDP.py
@@ -0,0 +1,11 @@
+import socket
+
+HOST='localhost'
+PORT=5454
+
+instance=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+instance.bind((HOST,PORT))
+
+while True:
+    msg=instance.recv(40).decode()
+    print(msg)
